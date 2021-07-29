74c6967ca3f3126c5ecd0f7b73184daa4b080b0f
Gelson Matias <gelsonjmatias@gmail.com>
2021-07-28 23:55:17 -0300

    Exercicio finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import styled  from 'styled-components';
 import './App.css';
+// import Navbar from './navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Input = styled.input`
+  width: 400px;
+  height: 30px;
+  font-size: 16px;
+  padding: 8px;
+  
+`;
+
+function App(){
+
+  const [number, setNumber] = useState('')
+  const handleNumberImput = (e) =>{
+    setNumber(e.target.value)
+  }
+  const [gorjeta, setGorjeta] = useState('')
+  const handleGorjeta = (e) =>{
+    setGorjeta(e.target.value)
+  }
+  
+  const valGorjeta = Number((number*gorjeta)/100)
+  const total = Number(parseFloat(number) + valGorjeta)
+  return <>
+  <h1>Calculadora de gorjeta</h1>
+
+  <><p>Quanto deu a conta?</p></>
+  <Input type="text" value={number} onChange={handleNumberImput}/>
+
+  <><p>Qual a percentagem de gorjeta?</p></>
+  <Input type="text" value={gorjeta} onChange={handleGorjeta}/>
+
+   <>
+   {number.length > 0 ? <p>Sub total: <strong>R$ {number}</strong></p> : ''}
+   {number.length > 0 ? <p>Sub total: <strong>{` (${gorjeta}%): R$ ${valGorjeta}`}</strong></p> : ''}
+   {number.length > 0 ? <p><strong>{`Total: R$ ${total} `}</strong></p> : ''}
+   </>
+
+   </>
 }
 
 export default App;
